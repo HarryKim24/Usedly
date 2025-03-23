5c30cd43c475ff62e49b399bd6c7c9d1ee9596f6
HarryKim24 <tl9434@naver.com>
2025-03-23 15:46:34 +0900

    create UserPage, AdminPage

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+const AdminPage = () => {
+  return (
+    <div>
+      관리자만 볼 수 있는 페이지입니다.
+    </div>
+  )
+}
+
+export default AdminPage;
---
+++ src/app/user/page.tsx
@@ -0,0 +1,9 @@
+const UserPage = () => {
+  return (
+    <div>
+      로그인된 유저만 볼 수 있는 페이징입니다.
+    </div>
+  )
+}
+
+export default UserPage;
